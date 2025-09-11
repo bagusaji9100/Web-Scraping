546afd9f79cf34fa363b268a2c1d004342ab72d8
bagusaji9100 <bagusaji9100@gmail.com>
2025-09-11 09:13:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ st.markdown(
     """
     <style>
         /* ===== Global ===== */
-        .stApp, header[data-testid="stHeader"] { background: #FFF !important; color: #000 !important; border-bottom: 3px solid #e7dfdd !important; }
+        .stApp, header[data-testid="stHeader"] { background: #FFF !important; color: #000 !important; border-bottom: 3px solid #e7dfdd !important; height: 70px; }
         header[data-testid="stHeader"] *,
         .stMarkdown, .stText, .stTitle, .stSubheader, .stHeader, .stCaption,
         div[role="radiogroup"] * { color: #000 !important; }
